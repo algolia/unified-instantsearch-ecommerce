182eb0ce0744a65b53396a6d38f660ee9da56cf5
Valentin Polo <valentin.polo@algolia.com>
2019-04-29 16:55:12 +0200

    Add custom currentRefinements

--- src/CurrentRefinementsTags.js
+++ src/CurrentRefinementsTags.js
@@ -2,33 +2,29 @@ import React from 'react';
 
 import { connectCurrentRefinements } from 'react-instantsearch-dom';
 
-class CurrentRefinementsTags extends React.Component {
+const CurrentRefinementsTags = ({ items, refine }) => {
+    const tags = items.reduce((acc, curr) => (
+        [...acc, ...curr.items.map(refinement => ({
+            group: curr.attribute,
+            label: refinement.label,
+            value: refinement.value
+        }))]
+    ), []);
 
-    render() {
-        const { items, refine, createURL } = this.props;
-
-        const tags = items.reduce((acc, curr) => (
-            [...acc, ...curr.items.map(refinement => ({
-                group: curr.attribute,
-                label: refinement.label,
-                value: refinement.value
-            }))]
-        ), []);
-
-        return (
-            <ul className="euip-RefinementsTags">
-                {tags.map((tag, idx) => {
-                    return (
-                        <li key={idx}
-                            onClick={e => { e.preventDefault(); refine(tag.value); }}>
-                            {tag.label}
-                        </li>
-                    )
-                })}
-            </ul>
-        )
-    }
-
-}
+    return (
+        <ul className="euip-RefinementsTags">
+            {tags.map((tag, idx) => {
+                return (
+                    <li key={idx}
+                        onClick={e => { e.preventDefault(); refine(tag.value); }}
+                        className="euip-RefinementsTags-tag"
+                    >
+                        {tag.group}: <em>{tag.label}</em>
+                    </li>
+                )
+            })}
+        </ul>
+    )
+};
 
 export default connectCurrentRefinements(CurrentRefinementsTags);
