7e2de837e0076a75c99f0986c519808edecc394b
Guillaume DESSAIN <guillaume.dessain@epita.fr>
2019-05-23 15:01:18 +0200

    Fix tag (#16)
    * Fix tagging issue on rangeslider
    
    * Remove metadata from refinement tags
    
    * add comments

--- src/right-column/CurrentRefinementsTags.js
+++ src/right-column/CurrentRefinementsTags.js
@@ -3,12 +3,22 @@ import React from 'react';
 import { connectCurrentRefinements } from 'react-instantsearch-dom';
 
 const CurrentRefinementsTags = ({ items, refine }) => {
+    //This reduce loop over the items
+    //If the items key present then it's a currentRefinement type else it's a range
     const tags = items.reduce((acc, curr) => (
-        [...acc, ...curr.items.map(refinement => ({
-            group: curr.attribute,
-            label: refinement.label,
-            value: refinement.value
-        }))]
+        [...acc, ...("items" in curr ?
+            [...curr.items.map(refinement => ({
+                group: curr.attribute,
+                label: refinement.label.split(';')[0],
+                value: refinement.value
+            }))]
+            :
+            [{
+                group: curr.attribute,
+                label: `${curr.currentRefinement.min || "min"} to ${curr.currentRefinement.max || "max"}`,
+                value: curr.value
+            }]
+        )]
     ), []);
 
     return (
