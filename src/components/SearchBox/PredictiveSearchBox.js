61ca506cf43641f7a5b707d33125175154f0091b
François Chalifour <francois.chalifour@gmail.com>
2020-04-14 17:54:47 +0200

    fix(predictive): handle focus, blur and tab events

--- src/components/SearchBox/PredictiveSearchBox.js
+++ src/components/SearchBox/PredictiveSearchBox.js
@@ -11,13 +11,14 @@ import { ReverseHighlight } from './ReverseHighlight';
 import './PredictiveSearchBox.css';
 
 export const PredictiveSearchBox = connectSearchBox((props) => {
+  const [showSuggestion, setShowSuggestion] = React.useState(false);
   const [suggestion, setSuggestion] = React.useState(null);
 
   return (
     <div className="euip-searchBar">
       <div className="euip-searchBar-inner">
         <div className="ais-PredictiveSearchBox">
-          {suggestion && (
+          {showSuggestion && suggestion && (
             <div className="ais-PredictiveBox">
               <span id="predictive-item">{suggestion}</span>
             </div>
@@ -33,33 +34,42 @@ export const PredictiveSearchBox = connectSearchBox((props) => {
             autoFocus
             placeholder={props.translations.placeholder}
             className="ais-PredictiveSearchBox-input"
+            onFocus={() => setShowSuggestion(true)}
+            onBlur={() => setShowSuggestion(false)}
+            onKeyDown={(event) => {
+              if (event.key === 'Tab') {
+                event.preventDefault();
+                props.refine(suggestion);
+                return;
+              }
+            }}
           />
         </div>
         <span className="euip-searchBar-close" onClick={props.onClose}>
           ×
         </span>
 
-        {props.currentRefinement && (
-          <Index indexName={props.suggestionsIndexName}>
-            <Configure
-              facets={[]}
-              page={0}
-              hitsPerPage={props.maxSuggestions}
-            />
-            <Suggestions
-              query={props.currentRefinement}
-              onSuggestion={(suggestion) => setSuggestion(suggestion)}
-              onClick={(value) => props.refine(value)}
-            />
-          </Index>
-        )}
+        <Index indexName={props.suggestionsIndexName}>
+          <Configure
+            // @TODO: remove all refinements from parent index
+            facets={[]}
+            page={0}
+            hitsPerPage={props.maxSuggestions}
+          />
+          <Suggestions
+            query={props.currentRefinement}
+            onSuggestion={(suggestion) => setSuggestion(suggestion)}
+            onClick={(value) => props.refine(value)}
+          />
+        </Index>
       </div>
     </div>
   );
 });
 
 const Suggestions = connectHits((props) => {
-  if (props.hits.length === 0) {
+  if (props.query === '' || props.hits.length === 0) {
+    props.onSuggestion(null);
     return null;
   }
 
@@ -74,7 +84,7 @@ const Suggestions = connectHits((props) => {
         props.query + firstSuggestion.slice(props.query.length);
       props.onSuggestion(suggestion);
     }
-  }, [props.query]);
+  }, [props.hits]);
 
   return (
     <ol className="ais-SuggestionTagsContainer">
