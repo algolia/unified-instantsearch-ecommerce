e429b4b4dcc73ee428072c720797bac7dd9aad52
François Chalifour <francois.chalifour@gmail.com>
2020-04-15 22:35:27 +0200

    feat(colors): display all colors in a scrollable container

--- src/components/ColorList.js
+++ src/components/ColorList.js
@@ -4,11 +4,8 @@ import { connectRefinementList } from 'react-instantsearch-dom';
 import { UncontrolledSearchBox } from './SearchBox/UncontrolledSearchBox';
 
 export const ColorList = connectRefinementList((props) => {
-  const [isShowingMore, setIsShowingMore] = React.useState(false);
   const [query, setQuery] = React.useState('');
   const inputRef = React.useRef(null);
-
-  const items = isShowingMore ? props.items : props.items.slice(0, props.limit);
 
   return (
     <div className="ColorList ais-RefinementList">
@@ -48,60 +45,57 @@ export const ColorList = connectRefinementList((props) => {
         </div>
       )}
 
-      <ul className="ais-RefinementList-list">
-        {items.map((item) => {
-          const labelParts = item.label.split(';');
+      <div className="Unified-RefinementList-ListContainer">
+        {props.isFromSearch && props.items.length === 0 && (
+          <p>No colors found.</p>
+        )}
 
-          if (labelParts.length !== 2) {
-            throw new Error(
-              'The Color widget expects colors with the following format: "Aluminium;#7f8084".'
+        <ul className="ais-RefinementList-list">
+          {props.items.map((item) => {
+            const labelParts = item.label.split(';');
+
+            if (labelParts.length !== 2) {
+              throw new Error(
+                'The Color widget expects colors with the following format: "Aluminium;#7f8084".'
+              );
+            }
+
+            const [colorName, colorCode] = labelParts;
+
+            return (
+              <li
+                key={item.label}
+                className={[
+                  'ais-RefinementList-item',
+                  item.isRefined && 'ais-RefinementList-item--selected',
+                ]
+                  .filter(Boolean)
+                  .join(' ')}
+              >
+                <label className="ais-RefinementList-label">
+                  <input
+                    className="ais-RefinementList-checkbox"
+                    style={{
+                      backgroundColor: colorCode,
+                    }}
+                    type="checkbox"
+                    value={item.value}
+                    checked={item.isRefined}
+                    onChange={(event) => {
+                      event.preventDefault();
+                      props.refine(item.value);
+                    }}
+                  />
+                  <span className="ais-RefinementList-labelText">
+                    {colorName}
+                  </span>
+                  <span className="ais-RefinementList-count">{item.count}</span>
+                </label>
+              </li>
             );
-          }
-
-          const [colorName, colorCode] = labelParts;
-
-          return (
-            <li
-              key={item.label}
-              className={[
-                'ais-RefinementList-item',
-                item.isRefined && 'ais-RefinementList-item--selected',
-              ]
-                .filter(Boolean)
-                .join(' ')}
-            >
-              <label className="ais-RefinementList-label">
-                <input
-                  className="ais-RefinementList-checkbox"
-                  style={{
-                    backgroundColor: colorCode,
-                  }}
-                  type="checkbox"
-                  value={item.value}
-                  checked={item.isRefined}
-                  onChange={(event) => {
-                    event.preventDefault();
-                    props.refine(item.value);
-                  }}
-                />
-                <span className="ais-RefinementList-labelText">
-                  {colorName}
-                </span>
-                <span className="ais-RefinementList-count">{item.count}</span>
-              </label>
-            </li>
-          );
-        })}
-      </ul>
-
-      {props.showMore && props.items.length > props.limit && (
-        <button
-          onClick={() => setIsShowingMore((prevValue) => !prevValue)}
-          className="ais-RefinementList-showMore"
-        >
-          {isShowingMore ? '- View fewer colors' : '+ View more colors'}
-        </button>
-      )}
+          })}
+        </ul>
+      </div>
     </div>
   );
 });
