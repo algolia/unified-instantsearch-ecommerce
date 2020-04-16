bb98df53e5046e0ecdf629d75658c913b7f2fd1b
François Chalifour <francois.chalifour@gmail.com>
2020-04-16 10:40:22 +0200

    feat: scroll to top on page change

--- src/App.js
+++ src/App.js
@@ -1,6 +1,11 @@
 import React from 'react';
 import { createPortal } from 'react-dom';
-import { InstantSearch, Configure, SortBy } from 'react-instantsearch-dom';
+import {
+  InstantSearch,
+  Configure,
+  SortBy,
+  ScrollTo,
+} from 'react-instantsearch-dom';
 
 import config from './config';
 import { getUrlFromState, getStateFromUrl, createURL } from './router';
@@ -127,26 +132,28 @@ export function App(props) {
                     </div>
 
                     <div className="Unified-RightPanel">
-                      <header className="Unified-BodyHeader">
-                        <div className="Unified-BodyHeader-heading">
-                          <Stats />
-                          {config.sorts && config.sorts.length > 0 && (
-                            <div className="Unified-BodyHeader-sortBy">
-                              <span className="Unified-Label">Sort by</span>
-                              <SortBy
-                                items={config.sorts}
-                                defaultRefinement={config.sorts[0].value}
-                              />
-                            </div>
-                          )}
-                        </div>
-                        <CurrentRefinements />
-                      </header>
+                      <ScrollTo>
+                        <header className="Unified-BodyHeader">
+                          <div className="Unified-BodyHeader-heading">
+                            <Stats />
+                            {config.sorts && config.sorts.length > 0 && (
+                              <div className="Unified-BodyHeader-sortBy">
+                                <span className="Unified-Label">Sort by</span>
+                                <SortBy
+                                  items={config.sorts}
+                                  defaultRefinement={config.sorts[0].value}
+                                />
+                              </div>
+                            )}
+                          </div>
+                          <CurrentRefinements />
+                        </header>
 
-                      <main className="Unified-BodyContent">
-                        <Banner />
-                        <ProductList />
-                      </main>
+                        <main className="Unified-BodyContent">
+                          <Banner />
+                          <ProductList />
+                        </main>
+                      </ScrollTo>
                     </div>
                   </div>
                 </div>
