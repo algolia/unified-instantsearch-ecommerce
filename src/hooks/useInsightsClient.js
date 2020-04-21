fb343eac07e3b76e4ce4b14091277989a861ce2c
François Chalifour <francois.chalifour@gmail.com>
2020-04-21 19:13:49 +0200

    fix: only call `setUserToken` when needed

--- src/hooks/useInsightsClient.js
+++ src/hooks/useInsightsClient.js
@@ -1,26 +1,31 @@
 import React from 'react';
 import searchInsights from 'search-insights';
 
-export function useInsightsClient(appId, apiKey, userToken) {
-  return React.useMemo(() => {
-    // When Search Insights is available on the global scope (`window.aa`), it
-    // it's wrapped in a "functional interface" that calls the right method
-    // with the passed arguments.
-    // Since we use the ESM import to avoid users to load the library on their
-    // page, we need to wrap the methods calls in a similar functional interface.
-    // See https://github.com/algolia/search-insights.js/blob/76f8bcd6f0ff711465ea5eddf9852045352675e2/lib/_getFunctionalInterface.ts
-    const insightsClient = (eventName, ...eventsArguments) => {
-      if (eventName && typeof searchInsights[eventName] === 'function') {
-        searchInsights[eventName](...eventsArguments);
-      }
-    };
+// When Search Insights is available on the global scope (`window.aa`), it
+// it's wrapped in a "functional interface" that calls the right method
+// with the passed arguments.
+// Since we use the ESM import to avoid users to load the library on their
+// page, we need to wrap the methods calls in a similar functional interface.
+// See https://github.com/algolia/search-insights.js/blob/76f8bcd6f0ff711465ea5eddf9852045352675e2/lib/_getFunctionalInterface.ts
+const aa = (eventName, ...eventsArguments) => {
+  if (eventName && typeof searchInsights[eventName] === 'function') {
+    searchInsights[eventName](...eventsArguments);
+  }
+};
 
-    insightsClient('init', {
+export function useInsightsClient(appId, apiKey, userToken) {
+  React.useEffect(() => {
+    aa('init', {
       appId,
       apiKey,
     });
-    insightsClient('setUserToken', userToken);
+  }, [appId, apiKey]);
 
-    return insightsClient;
-  }, [appId, apiKey, userToken]);
+  React.useEffect(() => {
+    if (userToken) {
+      aa('setUserToken', userToken);
+    }
+  }, [userToken]);
+
+  return aa;
 }
