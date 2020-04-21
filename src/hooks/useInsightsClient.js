4ac08392e7c8e8754a8e0d73204f0e9afe6f6b02
François Chalifour <francois.chalifour@gmail.com>
2020-04-21 18:57:18 +0200

    refactor: use `setUserToken`

--- src/hooks/useInsightsClient.js
+++ src/hooks/useInsightsClient.js
@@ -2,29 +2,25 @@ import React from 'react';
 import searchInsights from 'search-insights';
 
 export function useInsightsClient(appId, apiKey, userToken) {
-  React.useEffect(() => {
-    searchInsights.init({
+  return React.useMemo(() => {
+    // When Search Insights is available on the global scope (`window.aa`), it
+    // it's wrapped in a "functional interface" that calls the right method
+    // with the passed arguments.
+    // Since we use the ESM import to avoid users to load the library on their
+    // page, we need to wrap the methods calls in a similar functional interface.
+    // See https://github.com/algolia/search-insights.js/blob/76f8bcd6f0ff711465ea5eddf9852045352675e2/lib/_getFunctionalInterface.ts
+    const insightsClient = (eventName, ...eventsArguments) => {
+      if (eventName && typeof searchInsights[eventName] === 'function') {
+        searchInsights[eventName](...eventsArguments);
+      }
+    };
+
+    insightsClient('init', {
       appId,
       apiKey,
     });
-  }, [appId, apiKey]);
+    insightsClient('setUserToken', userToken);
 
-  // When Search Insights is available on the global scope (`window.aa`), it
-  // it's wrapped in a "functional interface" that calls the right method
-  // with the passed arguments.
-  // Since we use the ESM import to avoid users to load the library on their
-  // page, we need to wrap the methods calls in a similar functional interface.
-  // See https://github.com/algolia/search-insights.js/blob/76f8bcd6f0ff711465ea5eddf9852045352675e2/lib/_getFunctionalInterface.ts
-  return (eventName, ...eventsArguments) => {
-    // We enhance the event params with the provided user token so that users
-    // don't have to pass it in their events.
-    const events = eventsArguments.map((eventParams) => ({
-      userToken,
-      ...eventParams,
-    }));
-
-    if (eventName && typeof searchInsights[eventName] === 'function') {
-      searchInsights[eventName](...events);
-    }
-  };
+    return insightsClient;
+  }, [appId, apiKey, userToken]);
 }
