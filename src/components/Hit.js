5500b66efb7d43eeff8938b359c8ffba5e02ef42
François Chalifour <francois.chalifour@gmail.com>
2020-04-21 16:52:11 +0200

    fix: add CSS classes to tags

--- src/components/Hit.js
+++ src/components/Hit.js
@@ -13,22 +13,24 @@ export function Hit({ hit, insights, view }) {
         })
       }
     >
-      <a href={`https://asos.com/${hit.url}`}>
+      <a href={`https://asos.com/${hit.url}`} className="uni-Hit-inner">
         <div className="uni-Hit-image">
           <img src={hit.image} alt={hit.description} />
         </div>
 
         <div className="uni-Hit-Body">
-          <header>
-            <h2>{[hit.brand, hit.gender].filter(Boolean).join(' · ')}</h2>
+          <header className="uni-Hit-header">
+            <h2 className="uni-Hit-category">
+              {[hit.brand, hit.gender].filter(Boolean).join(' · ')}
+            </h2>
 
-            <h1>
+            <h1 className="uni-Hit-title">
               <Highlight attribute="description" tagName="mark" hit={hit} />
             </h1>
           </header>
 
           {view === 'list' && (
-            <p>
+            <p className="uni-Hit-description">
               Lorem ipsum dolor sit amet, consectetur adipiscing elit.
               Suspendisse placerat lectus non dui suscipit cursus. Ut interdum
               ac nisi eget egestas.
