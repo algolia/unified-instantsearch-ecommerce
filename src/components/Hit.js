35f922a1f72c47a3e4ba62ccb18734c00824bee5
François Chalifour <francoischalifour@users.noreply.github.com>
2020-04-21 17:26:54 +0200

    fix(view): use grid layout for hits (#42)

--- src/components/Hit.js
+++ src/components/Hit.js
@@ -13,29 +13,31 @@ export function Hit({ hit, insights, view }) {
         })
       }
     >
-      <a href={`https://asos.com/${hit.url}`}>
+      <a href={`https://asos.com/${hit.url}`} className="uni-Hit-inner">
         <div className="uni-Hit-image">
           <img src={hit.image} alt={hit.description} />
         </div>
 
         <div className="uni-Hit-Body">
-          <header className="uni-Hit-Header">
-            <h2>{[hit.brand, hit.gender].filter(Boolean).join(' · ')}</h2>
+          <header className="uni-Hit-header">
+            <h2 className="uni-Hit-category">
+              {[hit.brand, hit.gender].filter(Boolean).join(' · ')}
+            </h2>
 
-            <h1>
+            <h1 className="uni-Hit-title">
               <Highlight attribute="description" tagName="mark" hit={hit} />
             </h1>
           </header>
 
           {view === 'list' && (
-            <p className="uni-Hit-Description">
+            <p className="uni-Hit-description">
               Lorem ipsum dolor sit amet, consectetur adipiscing elit.
               Suspendisse placerat lectus non dui suscipit cursus. Ut interdum
               ac nisi eget egestas.
             </p>
           )}
 
-          <footer className="uni-Hit-Footer">
+          <footer>
             <span className="uni-Hit-Currency">£</span>
             <strong>{hit.price.toLocaleString()}</strong>{' '}
           </footer>
