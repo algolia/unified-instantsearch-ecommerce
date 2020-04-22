1a8e397b84e58f55e8b79367f1af8824df903e1c
François Chalifour <francois.chalifour@gmail.com>
2020-04-22 13:40:31 +0200

    feat(hit): display like button for conversion

--- src/components/Hit.js
+++ src/components/Hit.js
@@ -42,7 +42,39 @@ export function Hit({ hit, insights, view }) {
             <strong>{hit.price.toLocaleString()}</strong>{' '}
           </footer>
         </div>
+
+        <div className="uni-Hit-Actions">
+          <button
+            title="Like this product"
+            className="uni-Hit-ActionButton"
+            onClick={(event) => {
+              event.preventDefault();
+              event.stopPropagation();
+
+              insights('convertedObjectIDsAfterSearch', {
+                eventName: 'Product Liked',
+              });
+            }}
+          >
+            <LikeIcon />
+          </button>
+        </div>
       </a>
     </article>
+  );
+}
+
+function LikeIcon(props) {
+  return (
+    <svg
+      viewBox="0 0 24 24"
+      width={20}
+      height={20}
+      stroke="currentColor"
+      strokeWidth={2}
+      {...props}
+    >
+      <path d="M12 21.35l-1.45-1.32C5.4 15.36 2 12.28 2 8.5 2 5.42 4.42 3 7.5 3c1.74 0 3.41.81 4.5 2.09C13.09 3.81 14.76 3 16.5 3 19.58 3 22 5.42 22 8.5c0 3.78-3.4 6.86-8.55 11.54L12 21.35z" />
+    </svg>
   );
 }
