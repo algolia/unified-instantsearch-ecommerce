0b5d2f8d183b35470ff9e5a0ca2a35663ade2b90
Sarah Dayan <sarah.dayan@algolia.com>
2020-04-22 17:51:28 +0200

    fix: fix SVG

--- src/components/FiltersButton.js
+++ src/components/FiltersButton.js
@@ -5,9 +5,7 @@ import { FilterIcon } from './FilterIcon';
 export function FiltersButton() {
   return (
     <button className="uni-FiltersButton">
-      <svg xmlns="http://www.w3.org/2000/svg" viewbox="0 0 16 14">
-        <FilterIcon />
-      </svg>
+      <FilterIcon />
       Filters
     </button>
   );
