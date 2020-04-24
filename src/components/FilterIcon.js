867544d41cfcacd8a98e3e5a7d6684bae21df512
Sarah Dayan <sarah.dayan@algolia.com>
2020-04-24 15:52:59 +0200

    fix: remove unnecessary attribute

--- src/components/FilterIcon.js
+++ src/components/FilterIcon.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 export function FilterIcon(props) {
   return (
-    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 16 14" {...props}>
+    <svg viewBox="0 0 16 14" {...props}>
       <path
         d="M15 1H1l5.6 6.3v4.37L9.4 13V7.3z"
         stroke="currentColor"
