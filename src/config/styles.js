fb94eeda4c0276d80700219cc8d1d057ffc51191
Sarah Dayan <sarah.dayan@algolia.com>
2020-04-30 19:58:35 +0200

    feat: add styles configuration file

---
+++ src/config/styles.js
@@ -0,0 +1,17 @@
+const styles = {
+  colors: {
+    primary: '#d02139',
+    secondary: '#21243d',
+  },
+  text: {
+    fontFamily: `-apple-system, blinkmacsystemfont, 'Segoe UI', roboto, oxygen,
+ubuntu, cantarell, 'Open Sans', 'Helvetica Neue', sans-serif`,
+  },
+  breakpoints: {
+    sm: 640,
+    md: 768,
+    lg: 1024,
+  },
+};
+
+module.exports = styles;
