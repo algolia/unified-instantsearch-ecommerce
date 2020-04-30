32ba98704f1e591a343a4dc1a4ab78044e5d9f78
Sarah Dayan <sarah.dayan@algolia.com>
2020-04-30 20:00:17 +0200

    feat: include root file for custom properties

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { BrowserRouter as Router, Route } from 'react-router-dom';
 
+import './root.css';
 import './reset.scss';
 import './theme.scss';
 import './App.scss';
