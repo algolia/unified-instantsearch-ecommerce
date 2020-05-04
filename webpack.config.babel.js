95646f5da31c5fd096c0bca1ebabec9f7d928e56
Sarah Dayan <sarah.dayan@algolia.com>
2020-05-04 15:00:59 +0200

    feat: configure output management

--- webpack.config.babel.js
+++ webpack.config.babel.js
@@ -2,6 +2,8 @@ import path from 'path';
 import TerserJSPlugin from 'terser-webpack-plugin';
 import OptimizeCSSAssetsPlugin from 'optimize-css-assets-webpack-plugin';
 import MiniCssExtractPlugin from 'mini-css-extract-plugin';
+import HtmlWebpackPlugin from 'html-webpack-plugin';
+import { CleanWebpackPlugin } from 'clean-webpack-plugin';
 
 export default {
   entry: './src/index.js',
@@ -12,7 +14,14 @@ export default {
   optimization: {
     minimizer: [new TerserJSPlugin({}), new OptimizeCSSAssetsPlugin({})],
   },
-  plugins: [new MiniCssExtractPlugin()],
+  plugins: [
+    new MiniCssExtractPlugin(),
+    new CleanWebpackPlugin(),
+    new HtmlWebpackPlugin({
+      title: 'E-Comm Unified UI',
+      template: 'public/index.html',
+    }),
+  ],
   module: {
     rules: [
       {
