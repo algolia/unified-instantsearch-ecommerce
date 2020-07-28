d224b3d08b33c0e0ee561c22391d6abb6853217f
Sarah Dayan <sarah.dayan@algolia.com>
2020-07-28 17:59:49 +0200

    fix: define  to avoid breakage when using it in the config file

--- postcss.config.js
+++ postcss.config.js
@@ -1,5 +1,9 @@
 /* eslint-disable import/no-commonjs */
 
+// When the configuration file refers to `window`, we need to shim it so it
+// doesn't break when processed with Node during the build step.
+window = global;
+
 require('ignore-styles');
 const postcssPresetEnv = require('postcss-preset-env');
 const postcssInjectCssVariables = require('postcss-inject-css-variables');
